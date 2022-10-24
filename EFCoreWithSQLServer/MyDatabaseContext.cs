b5a8856086341c752491e5ffa2eef48e68810b83
AttaherFit <attaherhaboubacar1@gmail.com>
2022-10-24 23:06:54 +0100

    Add project files.

---
+++ EFCoreWithSQLServer/MyDatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using EFCoreWithSQLServer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreWithSQLServer
+{
+    public class MyDatabaseContext : DbContext
+    {
+        //DbSet<T> properties map to tables in the databases
+
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = "Data Source=.;" +
+                    "Initial Catalog=Northwind;" +
+                    "Integrated Security=true;" +
+                    "MultipleActiveResultSets=true;";
+            optionsBuilder.UseSqlServer(connection);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                        .Property(category => category.CategoryName)
+                        .IsRequired() // NOT NULL
+                        .HasMaxLength(15);
+        }
+    }
+}
---
+++ EFCoreWithSQLServer/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Function that queries all categories
+
+using EFCoreWithSQLServer;
+using EFCoreWithSQLServer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Diagnostics;
+
+
+static void GetCategories()
+{
+    using(MyDatabaseContext db = new())
+    {
+        Console.WriteLine("Categories and number of products they have :");
+        IQueryable<Category>? categories = db.Categories?.Include(category => category.Products);
+        if(categories == null)
+        {
+            Console.WriteLine("The table is empty");
+            return;
+        }
+
+        foreach(Category category in categories)
+        {
+            Console.WriteLine($"{category.CategoryName} has [ {category.Products?.Count} ] producs");
+        }
+    }
+}
+static void GetProducts()
+{
+    using (MyDatabaseContext db = new())
+    {
+        IQueryable<Product>? products = db.Products;
+        if(products == null)
+        {
+            Console.WriteLine("No product found");
+            return;
+        }
+        foreach(Product product in products)
+        {
+            Console.WriteLine("Available Products are :");
+            Console.WriteLine($"{product.ProductId}. {product.ProductName}");
+        }
+    }
+}
+static bool InsertProduct(int categoryId, string productName)
+{
+    using(MyDatabaseContext db = new())
+    {
+        Product p = new()
+        {
+            CategoryId = categoryId,
+            ProductName = productName,
+            //Cost = price
+        };
+        db.Products.Add(p);
+
+        // save tracked change to database
+        int affected = db.SaveChanges();
+        return(affected == 1);
+    }
+}
+
